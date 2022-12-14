91d14841714383bdf863b694c35b12944c9a2e76
Laymont Arratia <laymont@gmail.com>
2022-12-14 15:57:16 -0400

    fixeds

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.JEKYLL_ENV == 'production'
-      ? [require('cssnano')({ preset: 'default' })]
+    require("autoprefixer"),                                                    // example of plugin you might use
+    ...(process.env.JEKYLL_ENV == "production"                                  // example of only using a plugin in production
+      ? [require("cssnano")({ preset: "default" })]
       : [])
-  ],
+  ]
 }
